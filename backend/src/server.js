bb7f0bb364c8d903691d76f700f5c41f44a93c65
Dhruvbindal288 <dhruvbindal288@gmail.com>
2025-08-22 14:38:55 +0530

    basic setp done in backend

---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import authRoutes from "../src/routes/auth.route.js"
+dotenv.config()
+const app=express();
+
+app.use("/api/auth/",authRoutes);
+
+
+const PORT=process.env.PORT;
+app.listen(PORT,()=>{
+    console.log("App started")
+})
