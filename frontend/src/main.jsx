592e033c36bdde31edae143f1a1e2b769719f5e2
Dhruvbindal288 <dhruvbindal288@gmail.com>
2025-08-26 18:58:48 +0530

    Setup axios

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router'
+import {  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+//Used to create client
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
 <StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
+   </QueryClientProvider>
   </StrictMode>
   </BrowserRouter>,
 )
