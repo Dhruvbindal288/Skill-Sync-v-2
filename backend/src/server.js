b4a196e1ace46286a12491628faf59c209dbf99c
Dhruvbindal288 <dhruvbindal288@gmail.com>
2025-08-27 14:29:24 +0530

    Basic authentication checks have been made

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,12 +6,16 @@ import cookieParser from 'cookie-parser'
 import authRoutes from "../src/routes/auth.route.js"
 import userRoutes from "../src/routes/user.route.js"
 import chatRoutes from '../src/routes/chat.route.js'
+import cors from 'cors'
 
 dotenv.config()
 const app=express();
 app.use(cookieParser())
 app.use(express.json());
-
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
-import {Routes,Route} from 'react-router'
+import {Routes,Route ,Navigate} from 'react-router'
 import HomePage from '../src/pages/HomePage'
 import SignUpPage from '../src/pages/SignUpPage'
 import LoginPage from '../src/pages/LoginPage'
@@ -8,18 +9,29 @@ import CallPage from '../src/pages/CallPage'
 import ChatPage from '../src/pages/ChatPage'
 import OnBoardingPage from '../src/pages/OnBoardingPage'
 import {Toaster} from 'react-hot-toast'
+import {useQuery} from "@tanstack/react-query"
+import { axiosinstance } from './lib/axios'
 function App() {
+  const{data:authData,isloading,error}=useQuery({
+queryKey:['authUser'],
+queryFn:async()=>{
+  const res=await axiosinstance.get("/auth/me")
+  return res.data;
+},retry:false
+  })
+
+  const authUser=authData?.user
   return (
     <div>
       <Toaster/>
       <Routes>
-<Route path='/' element={<HomePage/>}/>
-<Route path='/signup' element={<SignUpPage/>}/>
-<Route path='/login' element={<LoginPage/>}/>
-<Route path='/notifications' element={<NotificationsPage/>}/>
-<Route path='/call' element={<CallPage/>}/>
-<Route path='/chat' element={<ChatPage/>}/>
-<Route path='/onboarding' element={<OnBoardingPage/>}/>
+<Route path='/' element={authUser?<HomePage/>:<Navigate to='/login'/>}/>
+<Route path='/signup' element={!authUser?<SignUpPage/>:<Navigate to='/'/>}/>
+<Route path='/login' element={!authUser?<LoginPage/>:<Navigate to='/'/>}/>
+<Route path='/notifications' element={authUser?<NotificationsPage/>:<Navigate to='/login'/>}/>
+<Route path='/call' element={authUser?<CallPage/>:<Navigate to='/login'/>}/>
+<Route path='/chat' element={authUser?<ChatPage/>:<Navigate to='/login'/>}/>
+<Route path='/onboarding' element={authUser?<OnBoardingPage/>:<Navigate to='/login'/> }/>
       </Routes>
     </div>
   )
